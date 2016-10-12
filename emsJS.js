77f34bff7b3f6fb09078ab3c3be650aebb3f79c7
Tomer Caspi <tomer_c@optimove.com>
2016-10-12 15:44:07 +0300

    Loading employee from localstorage

--- emsJS.js
+++ emsJS.js
@@ -5,32 +5,59 @@
 
 var employeeArray = [];
 var countIDs = 0;
-// Returns array of objects of employees
-function getEmployees(){
-    var arr = [];
 
-    return arr;
-}
+var Store = {
+    key: "employees",
+    save: function (data){
+        localStorage.setItem(this.key, JSON.stringify(data));
+    },
+    load: function (){
+        var localStorageItem = localStorage.getItem(this.key);
+
+        /* If there are items in the Localstorage, then I'm taking their objects and add them
+         * to the employeeArray */
+        if (localStorageItem){
+            var arr = JSON.parse(localStorageItem).entries;
+            for (var i=0; i< arr.length; i++){
+                employeeArray.push(arr[i]);
+            }
+        }
+    }
+};
+
+// Returns array of objects of employees from the local store
+(function loadEmployees(){
+    Store.load();
+    // Initialize the countID's with the last object ID
+    if (employeeArray.length){
+        countIDs = employeeArray[employeeArray.length-1].id;
+        createEmployeesList();
+    }
+}());
+
+// loadEmployees();
 
 
 var buttonAddEmployee = document.getElementById("addEmployeeButton");
-buttonAddEmployee.addEventListener("click",setEmployee);
+buttonAddEmployee.addEventListener("click",getEmployee);
 
-function setEmployee(e){
-    // var employeeName = document.querySelector("input[name='employeeName']");
-    // var employeeSkill = document.querySelector("input[name='employeeSkill']");
-    // var selectField = document.querySelector("select[name='selectTitle']");
-    //var errorElement = document.querySelector("#error");
+/**
+ * Get values from a form
+ **/
+function getEmployee(e){
 
     var empName = document.querySelector("input[name='employeeName']").value;
     var empSkill = document.querySelector("input[name='employeeSkill']").value;
-    var select = document.querySelector("select[name='selectTitle']")
+    var select = document.querySelector("select[name='selectTitle']");
     var option = select.options[select.selectedIndex].text;
 
     if (!empName || !empSkill){
-        alert("You must enter employee Name or Employee Skill");
+        alert("You must fill all fields and then add an employee");
+        e.preventDefault();
+        return;
     }
 
+    /*  Creating an employee object and add it to an Array */
     var newObj = {
         id: ++countIDs,
         name: empName,
@@ -41,59 +68,36 @@ function setEmployee(e){
     }
     e.preventDefault();
 
-    employeeArray.push(newObj);
-    createEmployeesList();
-
+    /* create the employee in the HTML */
+    createEmployeesList(newObj);
 }
 
-function createEmployeesList() {
+/**
+ * Display the new Object in HTML.
+ */
+function createEmployeesList(empObj) {
+
+    if (empObj){
+        employeeArray.push(empObj);
+    }
+
     var source = document.getElementById("employee-template");
     var template = Handlebars.compile(source.innerHTML);
     var context = {
         // entries: getEmployees(),
         entries: employeeArray
-
-
-        /* [
-         {
-         id: "123",
-         name: "Tomer Caspi",
-         skill: "Da Boss",
-         title: "Teacher",
-         datetime: "---",
-         actionName: "Check-In"
-         },
-         {
-         id: "124",
-         name: "Daniel Weiner",
-         skill: "Junior",
-         title: "Manager",
-         datetime: "---",
-         actionName: "Check-In"
-         },
-         {
-         id: "125",
-         name: "Sami veSusu",
-         skill: "Junior",
-         title: "CEO",
-         datetime: "---",
-         actionName: "Check-In"
-         }
-         ]*/
     };
 
+
     var htmlString = template(context);
-    console.log(htmlString);
     var htmlContainer = document.querySelector("#container");
     htmlContainer.innerHTML = htmlString;
+
+    /* Save in Local Storage */
+    Store.save(context);
 }
 
-var Store = {
-    key: "employees",
-    save: function (data){
-        localStorage.setItem(this.key, JSON.stringify(data));
-    }
-};
+
 
 function onSubmit (){
     // 1. Get values from a form
@@ -103,3 +107,31 @@ function onSubmit (){
     Store.save(data);
     // Store.load(<from local storage>);
 }
+
+
+/* [
+ {
+ id: "123",
+ name: "Tomer Caspi",
+ skill: "Da Boss",
+ title: "Teacher",
+ datetime: "---",
+ actionName: "Check-In"
+ },
+ {
+ id: "124",
+ name: "Daniel Weiner",
+ skill: "Junior",
+ title: "Manager",
+ datetime: "---",
+ actionName: "Check-In"
+ },
+ {
+ id: "125",
+ name: "Sami veSusu",
+ skill: "Junior",
+ title: "CEO",
+ datetime: "---",
+ actionName: "Check-In"
+ }
+ ]*/
